510e6e2ac964d3c682928eda57dda491d77f5333
resoliwan <resoliwan@naver.com>
2022-08-12 17:25:27 +0900

    letcode

---
+++ 12-seq-hacking/test_slice.py
@@ -0,0 +1,23 @@
+# >>> slice
+# <class 'slice'>
+# >>> dir(slice)
+# ['__class__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'indices', 'start', 'step', 'stop']
+# >>>
+slice.indices
+# 'ABCDE'[:10:2] is the same as 'ABCDE'[0:5:2] .
+
+print(slice(0, 10, 2))
+# slice(0, 10, 2)
+print(slice(0, 10, 2).indices(5))
+# (0, 5, 2)
+print(slice(-3).indices(5))
+
+# (0, 2, 1)
+
+t = (1, 2, [3, 4])
+t[2] += [5, 6]
+print(t)
+
+import dis
+
+dis.dis("a")
---
+++ leetcode2/test_search_insertion_positioy.py
@@ -0,0 +1,49 @@
+import math
+
+
+class Solution:
+    def searchInsert(self, nums, target):
+        i = -1
+        for i, val in enumerate(nums):
+            if val >= target:
+                return i
+
+        return i + 1
+
+    def searchInsert1(self, nums, target):
+        index = 0
+        while index < len(nums):
+            if nums[index] >= target:
+                break
+            index += 1
+
+        return index
+
+    def searchInsert2(self, nums, target):
+        low = 0
+        high = len(nums)
+        mid = low + math.ceil((low + high) / 2)
+
+        while low < mid < high:
+            mid = low + math.ceil((low + high) / 2)
+            midval = nums[mid]
+            if midval == target:
+                break
+            elif midval < target:
+                low = mid
+            elif midval > target:
+                high = mid
+
+        return mid
+
+
+def test_search_insert1():
+    s = Solution()
+    # t1 = dict(nums=[1, 3, 5, 6], target=5, output=2)
+    t2 = dict(nums=[1, 3, 5, 6], target=2, output=1)
+    # t3 = dict(nums=[1, 3, 5, 6], target=7, output=4)
+    # for t in [t1, t2, t3]:
+    for t in [t2]:
+        # assert t["output"] == s.searchInsert(t["nums"], t["target"])
+        # assert t["output"] == s.searchInsert1(t["nums"], t["target"])
+        assert t["output"] == s.searchInsert2(t["nums"], t["target"])
